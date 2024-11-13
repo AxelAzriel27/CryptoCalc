d65eed1ece3a72d75793d4cc5cd5a280bfb2887e
jeverlyro <ikadektresnajeverly78@gmail.com>
2024-11-13 22:41:01 +0800

    'Adding some changes'

--- src/App.js
+++ src/App.js
@@ -171,7 +171,10 @@ const App = () => {
       {/* Why Choose Us Section */}
       <section id="why-choose" className="why-choose-section">
         <h2>Why Choose This Website?</h2>
-        <img src="https://via.placeholder.com/800x400" alt="Why Choose Us" />
+        <img
+          src="https://wallpapercave.com/wp/wp4678496.jpg"
+          alt="Why Choose Us"
+        />
         <div className="why-choose-cards">
           <div className="why-choose-card">
             <FaRegSmileBeam className="why-choose-icon" />
