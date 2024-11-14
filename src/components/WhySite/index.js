a7e2c3bb9f0daeae36c845cdebb62cc189307ccc
AxelAzriel27 <4xelkopalit@gmail.com>
2024-11-14 22:46:07 +0800

    feat: Added loading when the data is not loaded

--- src/components/WhySite/index.js
+++ src/components/WhySite/index.js
@@ -1,11 +1,16 @@
-import { FaBitcoin, FaDollarSign, FaRegSmileBeam } from "react-icons/fa";
-import { MdOutlineSecurity } from "react-icons/md";
-import { useState, useEffect } from "react";
+import { useState, useEffect, CSSProperties } from "react";
 import { getDatabase, ref, onValue } from "firebase/database";
-import { db } from "../../config/firebase"; // Update with the correct path
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "#1ad1a7",
+};
 
 const Why = () => {
   const [whyData, setWhyData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const db = getDatabase();
@@ -14,10 +19,24 @@ const Why = () => {
     onValue(whyChooseRef, (snapshot) => {
       const data = snapshot.val();
       setWhyData(data);
+      setLoading(false);
     });
   }, []);
 
-  if (!whyData) return <p>Loading...</p>;
+  if (loading) {
+    return (
+      <div className="spinner-container">
+        <ClipLoader
+          color="#1ad1a7"
+          loading={loading}
+          cssOverride={override}
+          size={50}
+        />
+      </div>
+    );
+  }
+
+  if (!whyData) return <p>No data available.</p>;
 
   const { title, cards } = whyData;
 
