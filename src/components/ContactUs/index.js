cfe2a5ee821585f8a920a0c5a49c7c9f0bee2694
AxelAzriel27 <4xelkopalit@gmail.com>
2024-11-16 00:13:49 +0800

    feat: Added animation in components

--- src/components/ContactUs/index.js
+++ src/components/ContactUs/index.js
@@ -1,18 +1,20 @@
 import { FaGithub } from "react-icons/fa";
-
+import { Bounce } from "react-awesome-reveal";
 const ContactUs = () => {
   return (
-    <section id="contact" className="contact-section">
-      <h2>Contact Us</h2>
-      <a
-        href="https://github.com/AxelAzriel27/CryptoCalc"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="contact-icon"
-      >
-        <FaGithub /> GitHub
-      </a>
-    </section>
+    <Bounce>
+      <section id="contact" className="contact-section">
+        <h2>Contact Us</h2>
+        <a
+          href="https://github.com/AxelAzriel27/CryptoCalc"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="contact-icon"
+        >
+          <FaGithub /> GitHub
+        </a>
+      </section>
+    </Bounce>
   );
 };
 export default ContactUs;
